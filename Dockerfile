e34dda9eb9a1e9e0de78b01b532d48a7764b40bd
Dom <dominicraebeeson@gmail.com>
2026-02-03 20:22:04 +0000

    apparchive => appstorage

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:25-alpine
 
 EXPOSE 8080
-ARG WORK_DIR="/apparchive"
+ARG WORK_DIR="/appstorage"
 ARG UID=1337
 ARG USERNAME=ogsh
 
@@ -10,4 +10,4 @@ ADD . .
 RUN adduser --uid $UID --disabled-password --gecos "" $USERNAME && chown -R $UID $WORK_DIR
 USER $USERNAME
 
-ENTRYPOINT ["node", "build/apparchive.js"]
+ENTRYPOINT ["node", "build/appstorage.js"]
--- src/env.ts
+++ src/env.ts
@@ -2,11 +2,11 @@ import { parseEnvironmentVariables } from "@open-game-server-host/backend-lib";
 
 const parsedVariables = parseEnvironmentVariables([
     {
-        key: "APP_ARCHIVE_CONFIG_BRANCH",
+        key: "APP_STORAGE_CONFIG_BRANCH",
         defaultValue: "main"
     }
 ]);
 
-export function getAppArchiveConfigBranch(): string {
+export function getAppStorageConfigBranch(): string {
     return parsedVariables.get("APP_ARCHIVE_CONFIG_BRANCH")!;
 }
